8969e61d075794da944b49571222f35e68dbfe91
RAHULRAJ P V <rahulraj.cs22@duk.ac.in>
2023-10-01 02:10:41 +0530

    Add files via upload

---
+++ python day_5.py
@@ -0,0 +1,15 @@
+# Problem Statement: Write a program using a while loop to print all the numbers from 1 to 10. Create a list of names. 
+# Use a for loop to iterate over the list and print a personalized greeting for each name.
+
+#Write a program using a while loop to print all the numbers from 1 to 10.
+i = 0
+while (i<=10):
+    print(i)
+    i += 1
+
+#Create a list of names. 
+list = ["ram","john","rahul","sita","geeta","pavan","suresh","ramesh","sudhir","sudhanshu"]
+
+## Use a for loop to iterate over the list
+for n in list:
+    print(f"Hello,Good morning😊,{n}!")
